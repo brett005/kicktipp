a027cddc779fb2d895274cafd96fa33bf86044e7
Marcel Kleinfeller <marcel@oompf.de>
2017-09-11 15:45:51 +0200

    Complete rewrite. Using bwin.com as lone source for odds now.

--- kicktipp.py
+++ kicktipp.py
@@ -1,36 +1,88 @@
 #!/usr/bin/env python3
 
-import oddschecker, sys
+import urllib.request
+from bs4 import BeautifulSoup
 
-class bcolor:
-    TEAM1 = '\033[92m'
-    TEAM2 = '\033[93m'
-    ENDC  = '\033[0m'
+####################
+# HELPER FUNCTIONS #
+####################
 
-def run_app():
-    color_team1, color_team2, color_endc = bcolor.TEAM1, bcolor.TEAM2, bcolor.ENDC
-    if "--no-color" in sys.argv:
-        color_team1 = ""
-        color_team2 = ""
-        color_endc = ""
-    print("kicktipp oddschecker")
-    odds = oddschecker.Odds()
-    matches = odds.get_matches()
-    total_points = 0
-    for match in matches:
-        score_propabilities = odds.get_score_propabilities(match)
-        best_tipps = odds.find_best_tipp(score_propabilities)
-        total_points = total_points + best_tipps[0][1]
-        team0_name = match['teams'][0]
-        team0_name = " "*(37-len(team0_name)) + team0_name
+def load_soup(url):
+    return BeautifulSoup(urllib.request.urlopen(url).read().decode(), 'html.parser')
 
-        score_left, score_right = best_tipps[0][0][0], best_tipps[0][0][1]
-        if best_tipps[0][0][0] >= 4 and best_tipps[0][0][1] >= 4:
-            score_left = "-"
-            score_right = "-"
-        print("{}{}{} {}:{} {}{}{}".format(color_team1, team0_name, color_endc, score_left, score_right, color_team2, match['teams'][1], color_endc))
-    print(" "*36 + "--------")
-    print(" "*17 + "Expected Point Gain: +" + str(total_points))
+#####################################################################################
+
+MAIN_PAGE = "https://sports.bwin.com/de/sports/4/43/wetten/bundesliga"
 
 if __name__ == "__main__":
-    run_app()
+    print("kicktipp predictor, Copyright (c) 2017 Marcel Kleinfeller")
+
+    # PART 1: Finding next games
+    print("[1] Searching for bundesliga games ...")
+    soup = load_soup(MAIN_PAGE)
+    raw_matches = soup.select("a.mb-event-details-buttons__button-link")
+    matches = []
+    for raw_match in raw_matches:
+        href = raw_match["href"]
+        if href[:11] == "/de/sports/":
+            matches.append(href)
+    print("[2] Found {} upcoming matches ...".format(len(matches)))
+    # PART 2: Modifying match urls
+    for i in range(0, len(matches)):
+        match = matches[i]
+        match = match.replace("/wetten/", "/35/wetten/")
+        match = match.replace("/de/sports/events/4/43/", "https://sports.bwin.com/de/sports/events/4/43/")
+        matches[i] = match
+
+    # PART 3: Parsing odds
+    odds = []
+    cnt = 3
+    for match in matches:
+        m_odds = [] # Odds, just for this match
+        print("[{}] Parsing odds for match {}".format(cnt, match))
+        soup = load_soup(match)
+        soup = soup.select("table.marketboard-event-with-header__markets-list")[0]
+        buttons = soup.select("button.mb-option-button__button")
+        for button in buttons:
+            divs = button.select("div")
+            score = divs[0].string.strip()
+            odd   = divs[1].string.strip()
+            if len(score) == 3:
+                m_odds.append(((int(score[0]), int(score[2])), float(odd)))
+            else:
+                m_odds.append(((-1, -1), float(odd)))
+        odds.append((match, m_odds))
+        cnt = cnt + 1
+
+    # PART 4: Calculating best odds for each game
+    for match in odds:
+        print("[{}] Predicting results for match: {}".format(cnt, match[0]))
+        odds = match[1]
+        corr_factor = 0
+        # 4.1 Normalizing propabilities
+        for odd in odds:
+            corr_factor = corr_factor + 1/odd[1]
+        best_tip = "n/a"
+        best_odd = 0
+
+        for l in range(0, 5):
+            for r in range(0, 5):
+                rating = 0
+                for odd in odds:
+                    if odd[0] == (-1, -1):
+                        continue
+                    prop = 1/(odd[1] * corr_factor)
+                    ll = odd[0][0]
+                    rr = odd[0][1]
+                    
+                    if ll == l and rr == r:
+                        rating = rating + 4 * prop
+                    elif ll - rr == l - r:
+                        rating = rating + 3 * prop
+                    elif (ll > rr and l > r) or (ll == rr and l == r) or (ll < rr and l < r):
+                        rating = rating + 2 * prop
+                if rating > best_odd:
+                    best_tip = "{}:{}".format(l, r)
+                    best_odd = rating
+        print("----> {}".format(best_tip), best_odd)
+        cnt = cnt + 1
