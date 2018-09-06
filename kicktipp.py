2515a12c7b7bc840bae02ce5f6a560ebc7b8178c
Marcel Kleinfeller <marcel@oompf.de>
2018-09-06 15:30:52 +0200

    Is way faster now.

--- kicktipp.py
+++ kicktipp.py
@@ -1,88 +1,121 @@
 #!/usr/bin/env python3
 
 import urllib.request
+import threading
+import numpy
 from bs4 import BeautifulSoup
 
-####################
-# HELPER FUNCTIONS #
-####################
+class AsyncSoupLoader(threading.Thread):    
+    def run(self):
+        soup = load_soup(self.__url)
+        table = soup.find(class_="marketboard-event-with-header__markets-list")
+
+        # Match name
+        match_name = soup.find(class_="event-block__event-name").get_text().strip()
+
+        # Extract data
+        scores = []
+        probs = []
+        for button in table.select("tr.marketboard-options-row > td.mb-option-button > button.mb-option-button__button"):
+            score = button.find(class_="mb-option-button__option-name").get_text().strip()
+            payout = button.find(class_="mb-option-button__option-odds").get_text().strip()
+            scores.append(score)
+            probs.append(1/float(payout))
+
+        # Normalize data
+        s = sum(probs)
+        for i in range(0, len(probs)):
+            probs[i] = probs[i]/s
+
+        # Structure data
+        data = numpy.zeros((5, 5))
+        for i in range(0, len(scores)):
+            if len(scores[i]) == 3 and scores[i][1] == ":":
+                l_score = int(scores[i][0])
+                r_score = int(scores[i][2])
+                if l_score < 5 and r_score < 5:
+                    data.itemset((l_score, r_score), probs[i])
+
+        self.__data = {
+            "match_name" : match_name,
+            "match_data" : data,
+            "best_tips" : get_best_tips(data)
+        }
+    
+    def set(self, url):
+        self.__url = url
+
+    def get(self):
+        return self.__data
 
 def load_soup(url):
-    return BeautifulSoup(urllib.request.urlopen(url).read().decode(), 'html.parser')
+    return BeautifulSoup(urllib.request.urlopen(url).read().decode(), "html.parser")
 
-#####################################################################################
-
-MAIN_PAGE = "https://sports.bwin.com/de/sports/4/43/wetten/bundesliga"
-
-if __name__ == "__main__":
-    print("kicktipp predictor, Copyright (c) 2017 Marcel Kleinfeller")
-
-    # PART 1: Finding next games
-    print("[1] Searching for bundesliga games ...")
-    soup = load_soup(MAIN_PAGE)
-    raw_matches = soup.select("a.mb-event-details-buttons__button-link")
+def get_matches(soup):
     matches = []
-    for raw_match in raw_matches:
-        href = raw_match["href"]
-        if href[:11] == "/de/sports/":
+    for match in soup.select("a.mb-event-details-buttons__button-link"):
+        href = match["href"]
+        if(href[0] == "/"):
+            href = href.replace("/wetten/", "/35/wetten/")
+            href = href.replace("/de/sports/events/4/43/", "https://sports.bwin.com/de/sports/events/4/43/")
             matches.append(href)
-    print("[2] Found {} upcoming matches ...".format(len(matches)))
-    # PART 2: Modifying match urls
-    for i in range(0, len(matches)):
-        match = matches[i]
-        match = match.replace("/wetten/", "/35/wetten/")
-        match = match.replace("/de/sports/events/4/43/", "https://sports.bwin.com/de/sports/events/4/43/")
-        matches[i] = match
+    return matches
 
-    # PART 3: Parsing odds
-    odds = []
-    cnt = 3
-    for match in matches:
-        m_odds = [] # Odds, just for this match
-        print("[{}] Parsing odds for match {}".format(cnt, match))
-        soup = load_soup(match)
-        soup = soup.select("table.marketboard-event-with-header__markets-list")[0]
-        buttons = soup.select("button.mb-option-button__button")
-        for button in buttons:
-            divs = button.select("div")
-            score = divs[0].string.strip()
-            odd   = divs[1].string.strip()
-            if len(score) == 3:
-                m_odds.append(((int(score[0]), int(score[2])), float(odd)))
-            else:
-                m_odds.append(((-1, -1), float(odd)))
-        odds.append((match, m_odds))
-        cnt = cnt + 1
+def get_match_data(url_list):
+    threads = []
+    match_data = []
+    for url in url_list:
+        th = AsyncSoupLoader()
+        th.set(url)
+        threads.append(th)
+        th.start()
+    
+    for th in threads:
+        th.join()
+        match_data.append(th.get())
+    
+    return match_data
 
-    # PART 4: Calculating best odds for each game
-    for match in odds:
-        print("[{}] Predicting results for match: {}".format(cnt, match[0]))
-        odds = match[1]
-        corr_factor = 0
-        # 4.1 Normalizing propabilities
-        for odd in odds:
-            corr_factor = corr_factor + 1/odd[1]
-        best_tip = "n/a"
-        best_odd = 0
+def get_reward_matrix(lhs, rhs):
+    rewards = numpy.zeros((5, 5))
+    for i in range(0, 5):
+        for j in range(0, 5):
+            rew = 0
+            if lhs == i and rhs == j:
+                rew = 4
+            elif (lhs != rhs) and (lhs - rhs == i - j):
+                rew = 3
+            elif (lhs - rhs) * (i - j) > 0 or (lhs == rhs and i == j):
+                rew = 2
+            rewards.itemset((i, j), rew)
+    return rewards
 
-        for l in range(0, 5):
-            for r in range(0, 5):
-                rating = 0
-                for odd in odds:
-                    if odd[0] == (-1, -1):
-                        continue
-                    prop = 1/(odd[1] * corr_factor)
-                    ll = odd[0][0]
-                    rr = odd[0][1]
-                    
-                    if ll == l and rr == r:
-                        rating = rating + 4 * prop
-                    elif ll - rr == l - r:
-                        rating = rating + 3 * prop
-                    elif (ll > rr and l > r) or (ll == rr and l == r) or (ll < rr and l < r):
-                        rating = rating + 2 * prop
-                if rating > best_odd:
-                    best_tip = "{}:{}".format(l, r)
-                    best_odd = rating
-        print("----> {}".format(best_tip), best_odd)
-        cnt = cnt + 1
+def get_expected_rewards(match_data):
+    expected_rewards = numpy.zeros((5, 5))
+    for left_score in range(0, 5):
+        for right_score in range(0, 5):
+            rewards = get_reward_matrix(left_score, right_score)
+            expected_rewards = expected_rewards + match_data.item((left_score, right_score)) * rewards
+    return expected_rewards
+
+def get_best_tips(match_data):
+    exp_rewards = get_expected_rewards(match_data)
+    arr = numpy.ravel(exp_rewards)
+    best_tips = numpy.argsort(arr).tolist()[::-1]
+    for i in range(0, len(best_tips)):
+        best_tips[i] = numpy.unravel_index(best_tips[i], exp_rewards.shape)
+    return best_tips
+
+print("kick-predict 18.08")
+
+print("[1] Liste der Spiele abfragen.")
+
+main_page = load_soup("https://sports.bwin.com/de/sports/4/43/wetten/bundesliga")
+match_urls = get_matches(main_page)
+
+print("[2] Daten zu den Spielen abrufen.")
+match_data = get_match_data(match_urls)
+print()
+for match in match_data:
+    print("{}{}{}\t{}".format(match["match_name"], " "*(max(0, 50-len(match["match_name"]))), match["best_tips"][0], match["best_tips"][1]))
+    print()
